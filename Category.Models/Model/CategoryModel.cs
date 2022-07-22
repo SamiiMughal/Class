f6596405001be2c54b09b4841cdbe51ff1e316f8
SamiiMughal <samiaslam680@gmail.com>
2022-07-22 20:50:17 +0500

    first commit

---
+++ Category.Models/Model/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Category.Models.Model
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+}
---
+++ Category/Controllers/WebCategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Category.Models;
+using Fluent.Infrastructure.FluentModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Category.Controllers
+{
+    public class WebCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public WebCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var data = _db.Categories.ToList();
+            return View(data);
+        }
+        public IActionResult Create_Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create_Add(CategoryModel formdata)
+        {
+            var data = _db.Categories.Add(formdata);
+            _db.SaveChanges();
+            return View("Index");
+        }
+        //[HttpPost]
+        //public IActionResult Create(CategoryModel FormData)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _db.Categories.Add(FormData);
+        //        _db.SaveChanges();
+        //        return RedirectToAction();
+        //    }
+        
+    }
+}
